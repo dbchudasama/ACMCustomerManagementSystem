d330675fcb48586a9e00cd162f1da08939041312
Divyesh B Chudasama <divyesh@elastacloud.com>
2017-04-24 13:09:00 +0100

    First commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        //Constructor for Customer Class
+        public Customer ()
+        {
+
+        }
+
+        //Constructor for Customer with customerId as the parameter
+        public Customer(int customerId)
+        {
+            //'this' refers to the current instance of the customer object being used
+            this.CustomerId = customerId;
+        }
+
+        //Static variable declaration for instance count
+        public static int InstanceCount { get; set; }
+
+        //Last Name property
+        public string LastName { get; set; }
+
+        //First Name property
+        public string FirstName { get; set; }
+
+        //Email address property
+        public string EmailAddress { get; set; }
+        
+        //Customer ID property - Here limiting the set command so that only I can set this property. No code outside this class can set this property
+        //as this is a key property
+        public int CustomerId { get; private set; }
+
+        //Fullname of customer. No setter only a getter as nothing should be able to change the value of this property
+        public string FullName
+        {
+            get
+            {
+                //Adding in logic to test for whether ',' is needed if first or last name is empty (For automated testing code to pass)
+                string fullname = FirstName;
+                //If the last name is populated
+                if(!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        //If the first name is populated
+                        if(!string.IsNullOrWhiteSpace(fullname))
+                            {
+                                //Add a comma after it
+                                fullname += ", ";
+                            }
+                            //Now add the Last name
+                            fullname += LastName;
+                    }
+
+                //Return the fullname - First Name + Last Name
+                return fullname;
+            }
+        }
+
+        //Method 'Save' to save the defined customer
+        public bool Save()
+        {
+            return true;
+        }
+
+        //Method to retrieve A SPECIFIC customer - Using the customerId as a parameter
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        //Second Retrieve Method that returns a LIST of customers
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        //Method 'Validate' to make sure name is valid
+        public bool Validate()
+        {
+            var isValid = true;
+            
+            //If either first or last names are empty then validation will fail
+            if (string.IsNullOrWhiteSpace(FirstName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+
+        }
+    }
+}
---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange - Initialised variables
+            Customer customer = new Customer();
+            customer.FirstName = "Divyesh";
+            customer.LastName = "Chudasama";
+
+            string expected = "Divyesh, Chudasama";
+
+            //Actual - Actual expected outcome - Expecting Full Name to be returned
+            string actual = customer.FullName;
+
+            //Assert - Build the test
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+
+        public void FullNameFirstNameEmpty()
+        {
+            //Arrange
+            Customer customer = new Customer();
+            customer.LastName = "Chudasama";
+
+            string expected = "Chudasama";
+
+            //Actual
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void FullNameLastNameEmpty()
+        {
+            //Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Divyesh";
+
+            string expected = "Divyesh";
+
+            //Actual
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void StaticTest()
+        {
+            //Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bhavini";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Bhakti";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Bhagirath";
+            Customer.InstanceCount += 1;
+
+            //Actual
+
+            //Assert - Instance count should be equal to '3'
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+
+        public void ValidateValid()
+        {
+            //Arrange
+            var customer = new Customer();
+            customer.FirstName = "Bhavini";
+            customer.EmailAddress = "bhavini@abc.com";
+
+            var expected = true;
+
+            //Actual - Invoking the Validate() Method on this Customer object instance
+            var actual = customer.Validate();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+
+        public void ValidateMissingFirstName()
+        {
+            //Arrange
+            var customer = new Customer();
+            customer.EmailAddress = "bhavini@abc.com";
+
+            var expected = false;
+
+            //Actual - Invoking the Validate() Method on this Customer object instance
+            var actual = customer.Validate();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
